c6b9716daea074b3f4da3aa0cf1f478c5ce89e32
Vitor Nunciatelli <145514131+vnunciatelli@users.noreply.github.com>
2024-03-22 18:34:05 -0300

    update

--- smartregua.py
+++ smartregua.py
@@ -1,4 +1,4 @@
-#TESTE DE MODEM VIVO WNC
+#SMART RÉGUA
 #DEV BY vitor.nunciatelli@eletromidia.com.br
 #V1.1 - 27/02/2024
 # repositório GIT https://github.com/vnunciatelli/regua_inteligente.git
@@ -9,21 +9,33 @@ from time import sleep
 import sys
 
 def broad():
+
     interfaces = socket.getaddrinfo(host=socket.gethostname(), port=None, family=socket.AF_INET)
     allips = [ip[-1][0] for ip in interfaces]
 
-    #msg = b'teste broaderagem'
-    msg = str.encode(sys.argv[1])
+    msg1 = b'RMC'
+    msg2 = b'AD'
+    #msg = str.encode(sys.argv[1])
     while True:
 
         for ip in allips:
-            print(f'sending on {ip}')
+            print(f'sending {msg1} on {ip}')
             sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
             sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
             sock.bind((ip,0))
-            sock.sendto(msg, ("255.255.255.255", 44404))
+            sock.sendto(msg1, ("255.255.255.255", 44404))
             sock.close()
 
-        sleep(2)
-        
+        sleep(25)
+
+        for ip in allips:
+            print(f'sending {msg2} on {ip}')
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+            sock.bind((ip,0))
+            sock.sendto(msg2, ("255.255.255.255", 44404))
+            sock.close()
+
+        sleep(25)
+
 broad()
